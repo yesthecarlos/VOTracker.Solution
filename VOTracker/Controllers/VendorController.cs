41d61d654798c6084c54d796ab4be0bcaae419db
yesthecarlos <carlosmendez86@gmail.com>
2021-05-24 09:07:41 -0700

    update Order model and controller and Vendor Controller

--- VOTracker/Controllers/VendorController.cs
+++ VOTracker/Controllers/VendorController.cs
@@ -12,5 +12,42 @@ namespace TrackerApp.Controllers
       List<Vendor> allVendors=Vendor.GetAll();
       return View(allVendors);
     }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName)
+    {
+      Vendor newVendor= new Vendor(vendorName);
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
+    }
+
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string orderDescription)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(orderDescription);
+      foundVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = foundVendor.Orders;
+      model.Add("orders", vendorOrders);
+      model.Add("vendor", foundVendor);
+      return View("Show", model);
+    }
   }
 }
--- VOTracker/Models/Order.cs
+++ VOTracker/Models/Order.cs
@@ -4,5 +4,34 @@ namespace TrackerApp.Models
 {
   public class Order
   {
+    public string Description { get; set; }
+    public int Id { get; }
+    private static List<Order> _instances = new List<Order> { };
+    public int Price { get; }
+    public string Date { get; set; }
+
+    public Order(string description)
+    {
+      Description = description;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Price = 0;
+      Date = "";
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
   }
 }
